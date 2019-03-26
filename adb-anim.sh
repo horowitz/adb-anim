65172f75c4495f4481aa0398cc548544f8a74f46
Daniel Horowitz <danielhorowitzzz@gmail.com>
2019-03-26 20:33:16 +0100

    Use bash for adb-anim script

--- adb-anim.sh
+++ adb-anim.sh
@@ -1,7 +1,17 @@
-#/user/bin/env ruby
+#!/bin/sh
 
-if ARGV.length != 1
-    puts "use -e for enabling or -d for disabling animations"
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. Please use -d for disabling or -e for enabling"
+    exit 1
+fi
 
-arg = ARGV[0]
-`enable-anim`
+if [ $1 == '-d' ]
+   then
+     sh ./disable-anim.sh
+ elif [ $1 = '-e' ]
+     then
+     sh ./enable-anim.sh    
+ else
+   echo "No arguments supplied. Please use -d for disabling or -e for enabling"
+fi
