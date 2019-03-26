d306fcebf499ed0c578358ce2ff696d438faddaa
danielhorowitz <danielhorowitzzz@gmail.com>
2019-03-26 18:08:31 +0100

    Add scripts for enabling/disabling animation

---
+++ enable-anim.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+adb shell settings put global window_animation_scale 1
+adb shell settings put global transition_animation_scale 1
+adb shell settings put global animator_duration_scale 1
